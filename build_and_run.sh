09b07cee6cee6cda334ec81cd93563fb12f56847
Soralsei <kohiodef@gmail.com>
2024-05-27 08:56:42 +0200

    Add build context comparison in build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Default variables initialization
+builder="builder"
+target="real"
+tag=""
+rebuild=false
+gpu=false
+container="ros"
+ros_ip=""
+
 usage() {
     echo "Usage : build_and_run.sh [OPTIONS]
 Options :
@@ -10,6 +20,20 @@ Options :
     -g, --gpu                               Add GPU support, OPTIONAL, default: false
     -h, --help                              Show this message"
     exit 2
+}
+
+confirmed=
+
+confirm() {
+    read -p "$1 [Y/n] : " answer
+    while [ "${answer,,}" != "y" ] && [ "${answer,,}" != "n" ]; do
+        read -p "$1 [Y/n] : " answer
+    done
+    if [ "${answer,,}" = "y" ]; then
+        confirmed=true
+    else
+        confirmed=false
+    fi
 }
 
 if [[ $# = 0 ]]; then
@@ -32,15 +56,6 @@ for arg in "$@"; do
 done
 # Replace the input arguments with the converted ones
 set -- "${args[@]}"
-
-# Default variables initialization
-builder="builder"
-target="real"
-tag=""
-rebuild=false
-gpu=false
-container="ros"
-ros_ip=""
 
 # Input argument parsing
 while getopts t:b:hi:rgc:a: option; do
@@ -80,20 +95,44 @@ echo -e "Rebuild : $rebuild\n"
 # Check if the docker image with tag name $tag exists
 if docker image inspect $tag >/dev/null 2>&1; then
     echo "Image '$tag' exists locally"
+
+    # Compute the SHA256 checksum of the files copied to the Docker image as well as the Dockerfile
+    ## Uses perl to find raw COPY instructions (ie. copying build context files/dirs into the image) in the Dockerfile
+    ## And get their names.
+    ## Then, find all files in the extracted file/directory names and pipe their contents into sha256sum to get the sum
+    checksum_host=$(echo Dockerfile ; cat Dockerfile | perl -lne 'print $1 if m/^COPY (?!--)([^ ]+) .*/') | xargs -I@ sh -c "find @ -type f" | sort | xargs cat | sha256sum | cut -d' ' -f1
+    echo "$checksum_host"
+
+    # TODO : test this snippet
+    mkdir temp/
+    # We should probably check that the image is built with the same Dockerfile
+    # but for now, just assume the user won't give the name of a completely unrelated image
+    docker create --name files "$tag" && docker cp files:/opt/ros/rr100_ws/ ./temp/
+    cd temp/rr100_ws
+
+    # Same as above
+    checksum_container=$(echo Dockerfile ; cat Dockerfile | perl -lne 'print $1 if m/^COPY (?!--)([^ ]+) .*/') | xargs -I@ sh -c "find @ -type f" | sort | xargs cat | sha256sum | cut -d' ' -f1
+    rm -rf temp/
+
+    if [ "$checksum_host" -ne "$checksum_container" ]; then
+        confirm "Build context changed since last build of image, do you want ot rebuild it"
+        if [ "$confirmed" = false ]; then
+            echo "Aborting..."
+            exit 1
+        fi
+        rebuild=true
+    fi
 else
-    # Else if it doesn't, ask the user if they want to build it or not
-    read -p "Image '$tag' does not exist locally, do you want to build it [Y/n] : " confirm
-    while [ "${confirm,,}" != "y" ] && [ "${confirm,,}" != "n" ]; do
-        echo "$confirm"
-        read -p "Image '$tag' does not exist locally, do you want to build it [Y/n] : " confirm
-    done
-    if [ "${confirm,,}" = "n" ]; then
+    # else if it doesn't, ask the user if they want to build it or not
+    confirm "Image '$tag' does not exist locally, do you want to build it"
+    if [ "$confirmed" = false ]; then
         echo "Aborting..."
         exit 1
     fi
+    rebuild=true
 fi
 
-if [ "$rebuild" = true ] || [ "${confirm,,}" = "y" ]; then
+if [ "$rebuild" = true ]; then
     echo -e "Build target : $target"
     if [ "$target" != "real" ] && [ "$target" != "simulation" ]; then
         echo -e "\nError : target '$target' should be either 'real' or 'simulation'"
@@ -101,7 +140,6 @@ if [ "$rebuild" = true ] || [ "${confirm,,}" = "y" ]; then
     fi
 
     args=""
-
     # A ROS_IP has to be specified when using target 'real',
     # so that remote ROS nodes can contact us
     if [ "$ros_ip" ]; then
@@ -140,18 +178,19 @@ docker run --rm -ti \
     --volume "$XAUTH:$XAUTH" \
     --volume /tmp/.X11-unix/:/tmp/.X11-unix/ \
     --volume $HOME/.gazebo:/root/.gazebo/ \
-    --name $container $tag
+    --name $container \
+    $tag
 
 ## docker run options explanation : 
 # --rm : deletes the container when exited
 # -ti  : attaches a terminal to the container and puts it in interactive mode
-# --net host : Mount the hosts machine network setup inside the container
+# --net host : Use the host machine's network stack inside the container
 # --env DISPLAY=$DISPLAY : Set the container display to the same display as the host
 # --env QT_X11_NO_MITSHM=1 : Used to fix an error when running QT inside the container
 # --env XAUTHORITY=$XAUTH : Use the same X authority as host
 # --volume "$XAUTH:$XAUTH" : Mount your XAUTH inside the container
-# -v /tmp/.X11-unix/:/tmp/.X11-unix/ : Mount the hosts X socket inside the container
-# -v $(pwd)/.gazebo:/root/.gazebo/ : Mount the container's .gazebo cache directory 
+# -volume /tmp/.X11-unix/:/tmp/.X11-unix/ : Mount the hosts X socket inside the container
+# -volume $HOME/.gazebo:/root/.gazebo/ : Mount the container's .gazebo cache directory 
     # to the host to avoid downloading gazebo 3D models on each build/new container
-# --name $container $tag : Used to specify the container's name 
+# --name $container : Used to specify the container's name 
     # (useful for commands like docker exec <container-name> or docker cp)
